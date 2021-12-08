f8105693640e36476919ac3a327fe2e40833f658
unknown <dorra.ben.ahmed@gmail.com>
2021-12-08 14:43:45 +0100

    Consultation Front

---
+++ Gestion_Cabinet_Front/Services/IConsultationService.cs
@@ -0,0 +1,19 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Gestion_Cabinet_Front.Services
+{
+    public interface IConsultationService
+    {
+        Task<IEnumerable<Consultation>> GetConsultations();
+        Task<Consultation> GetConsultation(int id);
+        Task<HttpResponseMessage> UpdateConsultation(Consultation updatedConsultation);
+        Task<HttpResponseMessage> CreateConsultation(Consultation NewConsultation);
+        Task DeleteConsultation(int id);
+
+    }
+}
--- Gestion_Cabinet_Front/obj/Debug/net5.0/RazorDeclaration/_Imports.razor.g.cs
+++ Gestion_Cabinet_Front/obj/Debug/net5.0/RazorDeclaration/_Imports.razor.g.cs
@@ -13,70 +13,70 @@ namespace Gestion_Cabinet_Front
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\daoud\Documents\IPSAS\3Ginfo\.Net core\Projet\Gestion_Cabinet_Front\Gestion_Cabinet_Front\_Imports.razor"
+#line 1 "C:\Users\Ramy\Source\Repos\Gestion_Cabiner\Gestion_Cabinet_Front\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\daoud\Documents\IPSAS\3Ginfo\.Net core\Projet\Gestion_Cabinet_Front\Gestion_Cabinet_Front\_Imports.razor"
+#line 2 "C:\Users\Ramy\Source\Repos\Gestion_Cabiner\Gestion_Cabinet_Front\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\daoud\Documents\IPSAS\3Ginfo\.Net core\Projet\Gestion_Cabinet_Front\Gestion_Cabinet_Front\_Imports.razor"
+#line 3 "C:\Users\Ramy\Source\Repos\Gestion_Cabiner\Gestion_Cabinet_Front\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\daoud\Documents\IPSAS\3Ginfo\.Net core\Projet\Gestion_Cabinet_Front\Gestion_Cabinet_Front\_Imports.razor"
+#line 4 "C:\Users\Ramy\Source\Repos\Gestion_Cabiner\Gestion_Cabinet_Front\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\daoud\Documents\IPSAS\3Ginfo\.Net core\Projet\Gestion_Cabinet_Front\Gestion_Cabinet_Front\_Imports.razor"
+#line 5 "C:\Users\Ramy\Source\Repos\Gestion_Cabiner\Gestion_Cabinet_Front\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\daoud\Documents\IPSAS\3Ginfo\.Net core\Projet\Gestion_Cabinet_Front\Gestion_Cabinet_Front\_Imports.razor"
+#line 6 "C:\Users\Ramy\Source\Repos\Gestion_Cabiner\Gestion_Cabinet_Front\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\daoud\Documents\IPSAS\3Ginfo\.Net core\Projet\Gestion_Cabinet_Front\Gestion_Cabinet_Front\_Imports.razor"
+#line 7 "C:\Users\Ramy\Source\Repos\Gestion_Cabiner\Gestion_Cabinet_Front\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\daoud\Documents\IPSAS\3Ginfo\.Net core\Projet\Gestion_Cabinet_Front\Gestion_Cabinet_Front\_Imports.razor"
+#line 8 "C:\Users\Ramy\Source\Repos\Gestion_Cabiner\Gestion_Cabinet_Front\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\daoud\Documents\IPSAS\3Ginfo\.Net core\Projet\Gestion_Cabinet_Front\Gestion_Cabinet_Front\_Imports.razor"
+#line 9 "C:\Users\Ramy\Source\Repos\Gestion_Cabiner\Gestion_Cabinet_Front\_Imports.razor"
 using Gestion_Cabinet_Front;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\daoud\Documents\IPSAS\3Ginfo\.Net core\Projet\Gestion_Cabinet_Front\Gestion_Cabinet_Front\_Imports.razor"
+#line 10 "C:\Users\Ramy\Source\Repos\Gestion_Cabiner\Gestion_Cabinet_Front\_Imports.razor"
 using Gestion_Cabinet_Front.Shared;
 
 #line default
